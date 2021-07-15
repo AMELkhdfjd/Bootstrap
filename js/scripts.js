89c638f480d4c2960a3d71cb12db785a7c4a287e
AMELKHdfjd <ia_khamoum@esi.dz>
2021-07-15 14:22:11 +0200

    separating the script code in a js file

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval: 2000});
+    $('#carouselButton').click(function()
+    {
+        if($('#carouselButton').children("span").hasClass('fa-pause'))
+               {
+               $('#mycarousel').carousel('pause');
+               $('#carouselButton').children('span').removeClass('fa-pause');
+               $('#carouselButton').children('span').addClass('fa-play');
+
+               }
+         else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+               $('#mycarousel').carousel('cycle');
+               $('#carouselButton').children('span').removeClass('fa-play');
+               $('#carouselButton').children('span').addClass('fa-pause');
+
+
+           }
+    }); 
+    $('#login_botton').click(function(){
+        $('#loginModal').modal('show');
+    });
+    $(document).ready(function(){
+   $('#reserve_button').click(function(){
+       $('#reserve').modal('show');
+   });
+   });
+});
